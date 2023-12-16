8215c3cbb8c4b73c438904ea7a083121a3353878
fabiooo4 <fabioirimie@gmail.com>
2023-12-16 13:01:17 +0100

    aggiornato sis e verilog

---
+++ Architettura/verilog/lab5/es2/es2.sv
@@ -0,0 +1,48 @@
+module es2(
+  input rst, clk, i1, i2,
+  output reg o);
+  reg state = 2'b00;
+  reg nextState = 2'b00;
+
+  always @(posedge clk) begin: SEQ
+    if (rst) state = 2'b00;
+    else state = nextState;
+  end
+
+  always @(state, i1, i2) begin: COMB
+    case (state)
+      2'b00: // A
+        if (~i1 && ~i2) begin
+          nextState = 2'b01; // B
+          o = 1'b0;
+        end else begin
+          nextState = 2'b00; // A
+          o = 1'b0;
+        end
+      2'b01: // B
+        if (i1 && i2) begin
+          nextState = 2'b10; // C
+          o = 1'b0;
+        end else begin
+          nextState = 2'b00; // A
+          o = 1'b0;
+        end
+      2'b10: // C
+        if (~i1 && ~i2) begin
+          nextState = 2'b11; // D
+          o = 1'b0;
+        end else begin
+          nextState = 2'b00; // A
+          o = 1'b0;
+        end
+      2'b11: // D
+        if (~i1 && i2) begin
+          nextState = 2'b00; // A
+          o = 1'b1;
+        end else begin
+          nextState = 2'b10; // C
+          o = 1'b0;
+        end
+    endcase
+  end
+endmodule
