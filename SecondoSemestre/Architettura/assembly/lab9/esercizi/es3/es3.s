00e6ab36ce7766cc038cf19b47e64300936dbbe8
fabiooo4 <fabioirimie@gmail.com>
2024-04-12 15:40:41 +0200

    aggiornato assembly lab9 es3

--- SecondoSemestre/Architettura/assembly/lab9/esercizi/es3/es3.s
+++ SecondoSemestre/Architettura/assembly/lab9/esercizi/es3/es3.s
@@ -3,10 +3,15 @@
 
 .section .data
   n:
-  .long 2
+  .long 3
+
+  inputText:
+  .ascii "Inserisci un numero naturale fino a 12: "
+  inputTextLen:
+  .long . - inputText
 
   resultText:
-  .ascii "Il fattoriale è: "
+  .ascii "\nIl fattoriale è: "
   resultTextLen:
   .long . - resultText
 
@@ -16,15 +21,48 @@
 .section .bss
   nStr:
   .string ""
+  r:
+  .string ""
 
 .section .text
 .globl _start
   
+_start:
+  # Print input text
+  movl $4, %eax # write
+  movl $1, %ebx # stdout
+  leal inputText, %ecx # string
+  movl inputTextLen, %edx # string length
+  int $0x80
+
+  # Input number
+  movl $3, %eax # read
+  movl $0, %ebx # keyboard
+  leal nStr, %ecx # destination address
+  movl $9, %edx # string length
+  int $0x80
+
   movl n, %eax
   movl %eax, %ecx
 
   factorial:
-  decl %ecx
-  mulb %ecx
+  cmp $0, %ecx
+  je zero
+  cmp $1, %ecx
+  je endFactorial
 
-_start:
+  decl %ecx
+  mull %ecx
+  jmp factorial
+
+  zero: # Base case
+  movl $1, %eax
+
+  endFactorial:
+  # Result is in %eax
+
+  # Terminate
+  movl $1, %eax
+  movl $0, %ebx
+  int $0x80
+
