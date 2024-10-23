5e29cae7a4f3a949db92639c1c9577d72ffeb4ad
fabiooo4 <fabioirimie@gmail.com>
2024-10-23 10:03:28 +0200

    aggiornato Reti

---
+++ SecondoAnno/PrimoSemestre/Algoritmi/Implementazioni/target/debug/deps/insertion_sort-3ea81614eb50b41b.d
@@ -0,0 +1,5 @@
+/home/fabio/uni/SecondoAnno/PrimoSemestre/Algoritmi/Implementazioni/target/debug/deps/libinsertion_sort-3ea81614eb50b41b.rmeta: src/bin/insertion_sort.rs
+
+/home/fabio/uni/SecondoAnno/PrimoSemestre/Algoritmi/Implementazioni/target/debug/deps/insertion_sort-3ea81614eb50b41b.d: src/bin/insertion_sort.rs
+
+src/bin/insertion_sort.rs:
---
+++ SecondoAnno/PrimoSemestre/Algoritmi/Implementazioni/target/debug/insertion_sort.d
@@ -0,0 +1,1 @@
+/home/fabio/uni/SecondoAnno/PrimoSemestre/Algoritmi/Implementazioni/target/debug/insertion_sort: /home/fabio/uni/SecondoAnno/PrimoSemestre/Algoritmi/Implementazioni/src/bin/insertion_sort.rs
